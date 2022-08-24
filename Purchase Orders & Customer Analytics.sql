b04062e1975faf40bb82aff60889c5217c4e64d7
Maximiliano E. Lutz <58157505+iMakishi@users.noreply.github.com>
2022-08-24 17:20:08 -0300

    Create Purchase Orders & Customer Analytics.sql

---
+++ Purchase Orders & Customer Analytics.sql
@@ -0,0 +1,45 @@
+/*How many orders were placed in January?*/
+
+/*SELECT COUNT(orderID)
+FROM JanSales;*/
+
+/*How many of those orders were for an iPhone?*/
+
+/*SELECT COUNT(orderID)
+FROM JanSales
+WHERE Product LIKE 'iPhone%';*/
+
+/*Select the customer account numbers for all the orders that were placed in February.*/
+
+/*SELECT acctnum
+FROM BIT_DB.customers
+INNER JOIN BIT_DB.FebSales
+ON order_id = orderID*/
+
+/*Which product was the cheapest one sold in January, and what was the price? For this one, you are going to use the commands distinct and MIN(). Using 'SELECT distinct' will remove duplicate rows from your results. The MIN() command will allow you to select the smallest value from the price column. This is a hard one - see if you can do it, but don't be afraid to look at the hint or answer if you need to!*/
+
+/*SELECT Product, price
+FROM BIT_DB.JanSales
+ORDER BY price asc
+LIMIT 1*/
+
+/*What is the total revenue for each product sold in January? (Revenue can be calculated using the number of products sold and the price of the products).*/
+
+/*SELECT Product, sum(Quantity) * price AS revenue
+FROM BIT_DB.JanSales
+GROUP BY Product*/
+
+/*Which products were sold in February at 548 Lincoln St, Seattle, WA 98101, how many of each were sold, and what was the total revenue?*/
+
+/*SELECT Product, location, sum(Quantity) AS number_sold, sum(Quantity) * price AS revenue
+FROM BIT_DB.FebSales
+WHERE location = '548 Lincoln St, Seattle, WA 98101'
+GROUP BY Product*/
+
+/*How many customers ordered more than 2 products at a time in February, and what was the average amount spent for those customers?*/
+
+SELECT COUNT(customers.acctnum), AVG(Quantity * price) AS AVG_Spent
+FROM BIT_DB.FebSales FebSales
+LEFT JOIN BIT_DB.customers customers
+ON FebSales.orderID = customers.order_id
+WHERE FebSales.Quantity > 2
